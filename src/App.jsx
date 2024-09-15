f645bb09c82ac7ba449de4dc78983ad6f3e94c45
Parashar Das <porashorbabu@gmail.com>
2024-09-16 00:48:35 +0600

    fortfolio init done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+export default function App() {
+    return (
+      <h1 className="text-4xl font-bold underline bg-black py-10">
+        Hello world!
+      </h1>
+    );
+  }
